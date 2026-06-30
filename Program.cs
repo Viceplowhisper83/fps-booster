9d095e3b10a366525fe90b684827fa4ee94d6c8d
Viceplowhisper83 <afsarsinanoglu13928@gmail.com>
2026-06-30 14:47:28 +0300

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AppStub
+{
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            Console.WriteLine("Running...");
+            return 0;
+        }
+    }
+}
